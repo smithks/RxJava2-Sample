9cd503621ce01aeb273bce7d0c3a962a308cefda
Keegan Smith <keegan.smith@willowtreeapps.com>
2017-07-07 10:13:04 -0400

    subscibeOn() usage

--- rxjava2-sample/src/main/java/com/example/keegansmith/rxjava2_sample/MainActivity.java
+++ rxjava2-sample/src/main/java/com/example/keegansmith/rxjava2_sample/MainActivity.java
@@ -5,6 +5,7 @@ import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,43 +20,47 @@ public class MainActivity extends AppCompatActivity {
         //Branch-start
         //Additional Info
 
-        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
-            @Override
-            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
-                e.onNext(0);
-                e.onNext(1);
-                e.onNext(2);
-                e.onNext(3);
-                e.onNext(4);
+//        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+//            @Override
+//            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+//                e.onNext(0);
+//                e.onNext(1);
+//                e.onNext(2);
+//                e.onNext(3);
+//                e.onNext(4);
+//
+//                e.onComplete();
+//            }
+//        });
 
-                e.onComplete();
-            }
-        });
 
         Observer<Integer> observer = new Observer<Integer>() {
             @Override
             public void onSubscribe(Disposable d) {
-                Log.e(this.getClass().getSimpleName(),"onSubscribe");
+                Log.v(this.getClass().getSimpleName(),"onSubscribe on Thread: "+Thread.currentThread().getName());
             }
 
             @Override
             public void onNext(Integer integer) {
-                Log.e(this.getClass().getSimpleName(),"onNext: "+integer);
+                Log.v(this.getClass().getSimpleName(),"onNext: "+integer+" On Thread: "+Thread.currentThread().getName());
             }
 
             @Override
             public void onError(Throwable e) {
-                Log.e(this.getClass().getSimpleName(),"onError: "+e.getMessage());
+                Log.v(this.getClass().getSimpleName(),"onError: "+e.getMessage());
 
             }
 
             @Override
             public void onComplete() {
-                Log.e(this.getClass().getSimpleName(),"onComplete!");
+                Log.v(this.getClass().getSimpleName(),"onComplete!");
             }
         };
 
-        observable.subscribe(observer);
+        Log.v(this.getClass().getSimpleName(),"Assigning observer from "+Thread.currentThread().getName());
+        Observable.just(1,2,3)
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(observer);
 
     }
 }
