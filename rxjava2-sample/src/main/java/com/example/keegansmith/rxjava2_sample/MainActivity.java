fba0898d1ac5462d364fab31a5ba1668240433f1
Keegan Smith <keegan.smith@willowtreeapps.com>
2017-08-09 14:03:35 -0400

    Stuff from a different file

--- rxjava2-sample/src/main/java/com/example/keegansmith/rxjava2_sample/MainActivity.java
+++ rxjava2-sample/src/main/java/com/example/keegansmith/rxjava2_sample/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     //Test commit
     //ADding some stuff yo
     //This is a new commit ayoooooooo
+    //More text not in gradle file
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
